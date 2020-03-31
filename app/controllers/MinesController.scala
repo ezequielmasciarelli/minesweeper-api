bcda5dcfd08b6386b7e4a86bb3fc4f832cd41767
Ezequiel Masciarelli <ezequiel.masciarelli@despegar.com>
2020-03-31 16:33:03 -0300

    creo el mundo como un arreglo de 100 booleanos por ahora

--- app/controllers/MinesController.scala
+++ app/controllers/MinesController.scala
@@ -2,12 +2,21 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import scala.util.Random
 
 @Singleton
 class MinesController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index: Action[AnyContent] = Action {
-    Ok("Hello World")
+  val random: Random.type = scala.util.Random
+
+  def newGame: Action[AnyContent] = Action {
+    val worldWithMines = (0 to 20).foldLeft(Array.ofDim[Boolean](100))((world,_) => {
+      val indexOfPlacesWithoutMines = world.zipWithIndex.filter(_._1 == false).map(_._2).toList
+      val newMinePos = random.shuffle(indexOfPlacesWithoutMines).head
+      world(newMinePos) = true
+      world
+    })
+    Ok
   }
 
 }
