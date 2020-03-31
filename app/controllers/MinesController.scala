fff1bc29c1679a21507e0ca6ae03dc60ee410ab8
Ezequiel Masciarelli <ezequiel.masciarelli@despegar.com>
2020-03-31 15:03:12 -0300

    Hello World at /

--- app/controllers/MinesController.scala
+++ app/controllers/MinesController.scala
@@ -4,7 +4,7 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class MinesController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index: Action[AnyContent] = Action {
     Ok("Hello World")
