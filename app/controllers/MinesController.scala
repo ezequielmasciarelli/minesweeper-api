02b5161ea7b12a286bf817e8eb9094a4998be894
Ezequiel Masciarelli <ezequiel.masciarelli@despegar.com>
2020-03-31 18:39:23 -0300

    calculate adjacent mines

--- app/controllers/MinesController.scala
+++ app/controllers/MinesController.scala
@@ -10,15 +10,16 @@ import scala.util.Random
 class MinesController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   val random: Random.type = scala.util.Random
-  var worldWithMines : List[MineField] = initWorld
+  var worldWithMines : List[MineField]
+  var positionsWithMines : List[Int]
   case class MineField(hasMine:Boolean = false, discovered:Boolean = false, xPos:Int, yPos:Int)
 
     def initWorld : List[MineField] = {
       val allPositions = (1 to 100).toList
-      val positionsWithMines = random.shuffle(allPositions).take(20)
+      positionsWithMines = random.shuffle(allPositions).take(20)
       allPositions.foldLeft(List.empty[MineField])((world,act) => {
-        val xPos = act / 10
-        val yPos = act % 10
+        val xPos = act % 10
+        val yPos = act / 10
         if (positionsWithMines.contains(act)) MineField(hasMine = true, discovered = false, xPos, yPos) :: world
         else MineField(xPos = xPos,yPos = yPos) :: world
       })
@@ -31,9 +32,31 @@ class MinesController @Inject()(cc: ControllerComponents) extends AbstractContro
 
 
   case class PressPlaceRequest(xPos: Int, yPos:Int)
-  case class PressPlaceResponse(result: String)
+  case class PressPlaceResponse(alive: Boolean, revealAdjacentMinesSquare: Int)
   implicit val pressRequestRead: Reads[PressPlaceRequest] = Json.reads[PressPlaceRequest]
   implicit val pressResponseWrite: Writes[PressPlaceResponse] = Json.writes[PressPlaceResponse]
+
+  //returns a value that represent how far adjacents mines ARE (0,1,2,..)
+  def calculateAdjacentMines(xPos: Int, yPos: Int): Int = {
+    calculateAdjacentMinesRecursive(xPos,yPos,0)
+  }
+
+  def calculateAdjacentMinesRecursive(xPos: Int, yPos: Int, count: Int): Int = {
+    val possibleCoordinates: List[(Int,Int)] = List((xPos-1,yPos),(xPos-1,yPos-1),(xPos-1,yPos+1),(xPos,yPos+1),(xPos,yPos-1),(xPos+1,yPos+1),(xPos+1,yPos),(xPos-1,yPos+1))
+    val positionsWithMinesCord = positionsWithMines.map(each => {
+      val xPos = each % 10
+      val yPos = each / 10
+      (xPos,yPos)
+    })
+    val matchedCoordinates = positionsWithMinesCord.intersect(possibleCoordinates)
+    if(matchedCoordinates.nonEmpty) {
+      count
+    }
+    else {
+      calculateAdjacentMinesRecursive(xPos,yPos,count + 1)
+    }
+  }
+
   def pressPlace: Action[AnyContent] = Action { request =>
     request.body.asJson
       .map(_.as[PressPlaceRequest])
@@ -42,8 +65,11 @@ class MinesController @Inject()(cc: ControllerComponents) extends AbstractContro
           .filter(_.xPos == request.xPos)
           .find(_.yPos == request.yPos)
           .exists(_.hasMine)
-        if (hasMine) PressPlaceResponse("BOOM!")
-        else PressPlaceResponse("Safaste!")
+        if (hasMine) PressPlaceResponse(alive = false,0)
+        else {
+          val result : Int = calculateAdjacentMines(request.xPos,request.yPos)
+          PressPlaceResponse(alive = true,result)
+        }
       })
       .map(Json.toJson(_))
       .map(result => Ok(result))
